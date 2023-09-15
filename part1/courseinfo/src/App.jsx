4d439ac309d1400d6530add1edf6bf308595ac92
eemmtt <etmuzio@gmail.com>
2023-09-15 11:38:22 -0400

    part 1a

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function Header({course}) {
+  return(
+    <div>
+      <h1>{course}</h1>
+    </div>
+  );
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+function Part({name, exc}) {
+  return(
+    <p>{name} {exc}</p>
+  )
+}
+
+function Content({content}) {
+  let part1 = content.part1;
+  let part2 = content.part2;
+  let part3 = content.part3;
+  let exercises1 = content.exercises1;
+  let exercises2 = content.exercises2;
+  let exercises3 = content.exercises3;
+  return(
+    <div>
+      <Part name={part1} exc={exercises1}/>
+      <Part name={part2} exc={exercises2}/>
+      <Part name={part3} exc={exercises3}/>
+    </div>
+  );
+}
+
+function Total({excercises}){
+  let exercises1 = excercises.exercises1
+  let exercises2 = excercises.exercises2
+  let exercises3 = excercises.exercises3
+  return(
+    <div>
+      <p>
+        Number of exercises {exercises1 + exercises2 + exercises3}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course}/>
+      <Content content={{part1, exercises1, part2, exercises2, part3, exercises3}} />
+      <Total excercises={{exercises1, exercises2, exercises3}}/>
+    </div>
   )
 }
 
